7dc7b85ceba7bdc8550c59adbe6658ef97a50ab6
Marcus Gullstrand <magge.96@hotmail.com>
2018-03-19 23:13:02 +0100

    Done

--- Uppgift 9.2.py
+++ Uppgift 9.2.py
@@ -1,12 +1,16 @@
 import math
+import random
+import zlib
 
 txt = open("exempeltext.txt", "r").read()
 textByteArr = bytearray(txt, "UTF-8")
 
 print("Uppgift 1: ")
 print("Chars: {}".format(len(txt)))
-print("ByteArray size: {}".format(len(textByteArr)))
+print("textByteArr size: {}".format(len(textByteArr)))
 
+####################################################################
+print("__________________________")
 print("\nUppgift 2: ")
 
 def makeHisto(byteArr):
@@ -37,8 +41,49 @@ a = textByteArr
 b = makeHisto(a)
 c = makeProb(b)
 d = entropi(c)
-print( (len(txt) * d) / 8 )
+print("Entropy: {}".format(d))
+print("Expected compression: {}".format((len(txt) * d) / 8 ))
 
+####################################################################
+print("__________________________")
 print("\nUppgift 3: ")
 
-        
+theCopy = textByteArr[:]
+#print(theCopy.decode())
+random.shuffle(theCopy)
+#print(textByteArr.decode())
+#print(theCopy.decode())
+
+####################################################################
+print("__________________________")
+print("\nUppgift 4: ")
+
+zipCopy = zlib.compress(theCopy)
+zipText = zlib.compress(textByteArr)
+print("theCopy byte-array size: {}".format(len(theCopy)))
+print("theCopy bit-array size: {}".format((len(theCopy) * 8)))
+print("")
+print("ZipCopy byte-array size: {}".format(len(zipCopy)))
+print("ZipCopy bit-array size: {}".format((len(zipCopy) * 8)))
+print("Bit per byte (zipCopy/theCopy): {}".format((len(zipCopy) * 8) / len(theCopy)))
+print("")
+print("zipByteArr byte-array size: {}".format(len(zipText)))
+print("zipByteArr bit-array size: {}".format((len(zipText) * 8)))
+print("Bit per byte (zipByteArr/textByteArr): {}".format((len(zipText) * 8) / len(textByteArr)))
+
+####################################################################
+print("__________________________")
+print("\nUppgift 5: ")
+
+T1 = "Hoppas att denna laboration aldrig tar slut för nu börjar ett mycket intressant experiment!"
+T10 = T1*10
+
+zipT1 = zlib.compress(T1.encode())
+zipT10 = zlib.compress(T10.encode())
+
+print("zipT1 byte-array size: {}".format(len(zipT1)))
+print("zipT10 byte-array size: {}".format(len(zipT10)))
+
+
+
+
