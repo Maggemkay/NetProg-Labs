57e99a8278877197e0c4a8926d8c27ee24143265
Marcus Gullstrand <guma1502@student.ju.se>
2018-03-23 18:26:36 +0100

    not done

--- Uppgift 14.1.py
+++ Uppgift 14.1.py
@@ -1,26 +1,68 @@
 import sqlite3
 import urllib.request
 import re
+import codecs
+
+file = codecs.open("htmlfile.txt", "w", "utf-8")
 
 
+
+# Get the episodes and air-times
 page = urllib.request.urlopen("https://www.svtplay.se/kanaler?channel=tv6&date=2018-03-23")
-svtSite = page.read()
+svtPage = page.read().decode("utf-8")
 
+playListFromScript = re.findall(r"programs\":(.*?)\"schedules\":", str(svtPage))
+allSimp = re.findall(r"{[^}]*\"Simpsons\"}", str(playListFromScript))
+
+simpEpisode = []
+simpTime = []
+
+for episode in allSimp:
+    simpEpisode.append(re.findall(r"Säsong.*av \d\d\.", str(episode)))
+    simpTime.append(re.findall(r"\"publishingTime\":\"(.*)\",\"longDescription\"", str(episode)))
+
+
+
+# Make the Database
 conn = sqlite3.connect("tvDB")
 c = conn.cursor()
 
-#playList = re.findall(r"<ul class=\"play_guide-page-program-list-schedule\"(.*?)<div class=\"play_guide-channel-list__item--single play_guide-page-channel-chooser\">", str(svtSite))
-#<a(.*?)</a>
+# un-comment if you dont have table or you want to drop the current existing table
+# c.execute("CREATE TABLE simpsons(episode text, airTime text)")
+# c.execute("DROP TABLE simpsons")
 
-playListFromScript = re.findall(r"programs\":(.*?)\"schedules\":", str(svtSite))
-
-allSimpssons = re.findall(r"{[^}]*\"Simpsons\"}", str(playListFromScript))
-
-
+merge = []
+for i in range(0, len(simpEpisode)):
+    merge.append((simpEpisode[i], simpTime[i]))
 
 
+
+#c.execute("INSERT INTO simpsons VALUES (?, ?)", str(simpEpisode[i]) + str(simpTime[i]))
+
+
+
+
+
+
+
+# DEGUG
+# testArr = []
+# for episode in allSimp:
+#     testArr.append(episode)
+# for a in testArr:
+#     print(a)
 
 # Debug
-print(allSimpssons)
+#print(allSimpssons)
+
+for i in range(0, len(simpEpisode)):
+    print(str(simpEpisode[i]))
+    print(str(simpTime[i]) + "\n")
+  
+
+for i in range(0, len(simpEpisode)):
+    a = str(simpEpisode[i]) + "\n" + str(simpTime[i]) + "\n\n"
+    file.write(a)
+file.close()
 
 
